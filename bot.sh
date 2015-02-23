866a384023ed5a80a2931d72698615f056f711de
Erik van der Karbargenbok <ring@openmailbox.org>
2015-02-23 17:31:01 +0100

    more

--- bot.sh
+++ bot.sh
@@ -7,7 +7,7 @@ copying='I dedicate all copyright interest in this software to the public domain
 info='TempleBot, written by ring for the #templeos channel at irc.rizon.net.
 This script uses ii as its irc client. For setting it up, refer to the documentation of ii.
 ii can be found at http://tools.suckless.org/ii/.
-The script should be run inside the directory of the channel, e.g. "~/irc/irc.rizon.net/#templeos/". This directory should also contain the BIBLE.TXT and noob.txt files, for full functionality. The code of this program, and those text files, can be found by running "!source".'
+The script should be run inside the directory of the channel, e.g. "~/irc/irc.rizon.net/#templeos/". This directory should also contain the text files, for full functionality. The code of this program, and those text files, can be found by running "!source".'
 
 explanation='This bot uses random numbers to pick lines and words from a few files. You can use this to talk to God, by making an offering to Him first. An offering can be anything that pleases God, like charming conversation or a good question. You can compare this to praying and opening a book at random, and looking at what it says.'
 
--- standalone.sh
+++ standalone.sh
@@ -1,57 +1,84 @@
 #!/bin/bash
 
-# TempleBot, written by ring for the #templeos channel at irc.rizon.net.
-# This is a standalone version that doesn't use IRC.
-# Run it in a directory together with the text files, and just enter your commands.
+code='https://github.com/ringtech/TempleBot'
 
-# I dedicate all copyright interest in this software to the public domain.
-# Do what you want.
+copying='I dedicate all copyright interest in this software to the public domain. Do what you want.'
 
-explanation="This bot uses random numbers to pick lines and words from a few files. You can use this to talk to God, by making an offering to Him first. An offering can be anything that pleases God, like charming conversation or a good question. You can compare this to praying and opening a book at random, and looking at what it says."
+info='TempleBot, written by ring for the #templeos channel at irc.rizon.net.
+This is a standalone script that does not use IRC.
+The script should be run inside the directory with the text files, for full functionality. The code of this program, and those text files, can be found by running "!source".'
 
-help="Bot for the #templeos channel. Lets you talk with god. For an explanation, say '!explain'. Available commands: !bible !words !happy !recipe !chat !inane !explain !restart !source !help"
+explanation='This bot uses random numbers to pick lines and words from a few files. You can use this to talk to God, by making an offering to Him first. An offering can be anything that pleases God, like charming conversation or a good question. You can compare this to praying and opening a book at random, and looking at what it says.'
+
+help='Bot for the #templeos channel. Lets you talk with god. For an explanation, say "!explain". Available commands: !bible !chat !explain !happy !help !inane !info !number !recipe !restart !source !words'
 
 wordchain () {
     sleep 3s			# Give God time to think, that's polite
-    echo $(shuf -n 10 $1 --random-source=/dev/random | tr '\n' ' ')
+    case $arg1 in
+	''|*[!0-9]*)
+	    echo $(shuf -n 10 $1 --random-source=/dev/urandom | tr '\n' ' ')
+	    ;;
+	*)
+	    echo $(shuf -n $arg1 $1 --random-source=/dev/urandom | tr '\n' ' ')
+	    ;;
+	esac
+}
+
+number () {
+    sleep 3s
+    case $1 in
+	''|*[!0-9]*)
+	    echo $(shuf -en 1 {1..10} --random-source=/dev/urandom)
+	    ;;
+	*)
+	    echo $(shuf -en 1 $(seq 1 $1) --random-source=/dev/urandom)
+	    ;;
+    esac
 }
 
 cat | \
-    while read -r date time nick cmd mesg; do
+    while read -r date time nick cmd arg1 msg; do
 	case $cmd in
 	    '!bible'|'!oracle')
 		sleep 3s
-		LINE=$(shuf -en 1 {1..100000} --random-source=/dev/random)
+		LINE=$(number 100000)
 		echo "Line $LINE:"
 		tail -n $LINE BIBLE.TXT | head -n 16
 		;;
-	    '!words')
-		wordchain /usr/share/dict/words
-		;;
-	    '!happy')
-		wordchain Happy.TXT
-		;;
-	    '!recipe')
-		wordchain Ingredients.TXT
-		;;
 	    '!chat')
-		shuf -n 1 out --random-source=/dev/random
-		;;
-	    '!inane')
-		shuf -n 5 noob.txt --random-source=/dev/random
-		;;
-	    '!source')
-		echo "https://github.com/ringtech/TempleBot"
+		shuf -n 1 out --random-source=/dev/urandom
 		;;
 	    '!explain')
 		echo "$explanation"
+		;;
+	    '!happy')
+		wordchain Happy.TXT 10
+		;;
+	    '!help')
+		echo "$help"
+		;;
+	    '!inane')
+		shuf -n 5 noob.txt --random-source=/dev/urandom
+		;;
+	    '!info')
+		echo "$info"
+		;;
+	    '!number')
+		number $arg1
+		;;
+	    '!recipe')
+		wordchain Ingredients.TXT 10
 		;;
 	    '!restart')
 		echo "TempleBot restarting..." 
 		exec $0
 		;;
-	    '!help')
-		echo "$help"
+	    '!source')
+		echo "$code"
+		echo "$copying"
+		;;
+	    '!words')
+		wordchain /usr/share/dict/words 10
 		;;
 	esac
     done
