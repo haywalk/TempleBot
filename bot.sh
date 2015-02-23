5759403b555127ea14785225001f7bf01d47cbcd
Erik van der Karbargenbok <ring@openmailbox.org>
2015-02-23 17:05:05 +0100

    arguments, alphabetical sorting

--- bot.sh
+++ bot.sh
@@ -14,49 +14,56 @@
 
 explanation="This bot uses random numbers to pick lines and words from a few files. You can use this to talk to God, by making an offering to Him first. An offering can be anything that pleases God, like charming conversation or a good question. You can compare this to praying and opening a book at random, and looking at what it says."
 
-help="Bot for the #templeos channel. Lets you talk with god. For an explanation, say '!explain'. Available commands: !bible !words !happy !recipe !chat !inane !explain !restart !source !help"
+help="Bot for the #templeos channel. Lets you talk with god. For an explanation, say '!explain'. Available commands: !bible !chat !explain !happy !help !inane !recipe !restart !source !words"
 
 wordchain () {
     sleep 3s			# Give God time to think, that's polite
-    echo $(shuf -n 10 $1 --random-source=/dev/random | tr '\n' ' ')
+    case $arg1 in
+	''|*[!0-9]*)
+	    echo $(shuf -n 10 $1 --random-source=/dev/urandom | tr '\n' ' ')
+	    ;;
+	*)
+	    echo $(shuf -n $arg1 $1 --random-source=/dev/urandom | tr '\n' ' ')
+	    ;;
+	esac
 }
 
 tail -f -n 0 out | \
-    while read -r date time nick cmd mesg; do
+    while read -r date time nick cmd arg1 arg2 mesg; do
 	case $cmd in
 	    '!bible'|'!oracle')
 		sleep 3s
-		LINE=$(shuf -en 1 {1..100000} --random-source=/dev/random)
+		LINE=$(shuf -en 1 {1..100000} --random-source=/dev/urandom)
 		echo "Line $LINE:"
 		tail -n $LINE BIBLE.TXT | head -n 16
 		;;
-	    '!words')
-		wordchain /usr/share/dict/words
-		;;
-	    '!happy')
-		wordchain Happy.TXT
-		;;
-	    '!recipe')
-		wordchain Ingredients.TXT
-		;;
 	    '!chat')
-		shuf -n 1 out --random-source=/dev/random
-		;;
-	    '!inane')
-		shuf -n 5 noob.txt --random-source=/dev/random
-		;;
-	    '!source')
-		echo "https://github.com/ringtech/TempleBot"
+		shuf -n 1 out --random-source=/dev/urandom
 		;;
 	    '!explain')
 		echo "$explanation"
+		;;
+	    '!happy')
+		wordchain Happy.TXT 10
+		;;
+	    '!help')
+		echo "$help"
+		;;
+	    '!inane')
+		shuf -n 5 noob.txt --random-source=/dev/urandom
+		;;
+	    '!recipe')
+		wordchain Ingredients.TXT 10
 		;;
 	    '!restart')
 		echo "TempleBot restarting..." 
 		exec $0
 		;;
-	    '!help')
-		echo "$help"
+	    '!source')
+		echo "https://github.com/ringtech/TempleBot"
+		;;
+	    '!words')
+		wordchain /usr/share/dict/words 10
 		;;
 	esac
     done > in
