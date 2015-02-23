173c263a358add91bcf4cddf31c3e351a34dc603
Erik van der Karbargenbok <ring@openmailbox.org>
2015-02-23 17:26:51 +0100

    more things

--- bot.sh
+++ bot.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-# TempleBot, written by ring for the #templeos channel at irc.rizon.net.
-# This script uses ii as its irc client. For setting it up, refer to the
-# documentation of ii.
-# ii can be found at http://tools.suckless.org/ii/.
-# The script should be run inside the directory of the channel, e.g.
-# "~/irc/irc.rizon.net/#templeos/". This directory should also contain the
-# BIBLE.TXT and noob.txt files, for full functionality. They can be found
-# by running "!source".
+code='https://github.com/ringtech/TempleBot'
 
-# I dedicate all copyright interest in this software to the public domain.
-# Do what you want.
+copying='I dedicate all copyright interest in this software to the public domain. Do what you want.'
 
-explanation="This bot uses random numbers to pick lines and words from a few files. You can use this to talk to God, by making an offering to Him first. An offering can be anything that pleases God, like charming conversation or a good question. You can compare this to praying and opening a book at random, and looking at what it says."
+info='TempleBot, written by ring for the #templeos channel at irc.rizon.net.
+This script uses ii as its irc client. For setting it up, refer to the documentation of ii.
+ii can be found at http://tools.suckless.org/ii/.
+The script should be run inside the directory of the channel, e.g. "~/irc/irc.rizon.net/#templeos/". This directory should also contain the BIBLE.TXT and noob.txt files, for full functionality. The code of this program, and those text files, can be found by running "!source".'
 
-help="Bot for the #templeos channel. Lets you talk with god. For an explanation, say '!explain'. Available commands: !bible !chat !explain !happy !help !inane !recipe !restart !source !words"
+explanation='This bot uses random numbers to pick lines and words from a few files. You can use this to talk to God, by making an offering to Him first. An offering can be anything that pleases God, like charming conversation or a good question. You can compare this to praying and opening a book at random, and looking at what it says.'
+
+help='Bot for the #templeos channel. Lets you talk with god. For an explanation, say "!explain". Available commands: !bible !chat !explain !happy !help !inane !info !number !recipe !restart !source !words'
 
 wordchain () {
     sleep 3s			# Give God time to think, that's polite
@@ -28,12 +25,24 @@ wordchain () {
 	esac
 }
 
+number () {
+    sleep 3s
+    case $1 in
+	''|*[!0-9]*)
+	    echo $(shuf -en 1 {1..10} --random-source=/dev/urandom)
+	    ;;
+	*)
+	    echo $(shuf -en 1 $(seq 1 $1) --random-source=/dev/urandom)
+	    ;;
+    esac
+}
+
 tail -f -n 0 out | \
-    while read -r date time nick cmd arg1 arg2 mesg; do
+    while read -r date time nick cmd arg1 msg; do
 	case $cmd in
 	    '!bible'|'!oracle')
 		sleep 3s
-		LINE=$(shuf -en 1 {1..100000} --random-source=/dev/urandom)
+		LINE=$(number 100000)
 		echo "Line $LINE:"
 		tail -n $LINE BIBLE.TXT | head -n 16
 		;;
@@ -52,6 +61,12 @@ tail -f -n 0 out | \
 	    '!inane')
 		shuf -n 5 noob.txt --random-source=/dev/urandom
 		;;
+	    '!info')
+		echo "$info"
+		;;
+	    '!number')
+		number $arg1
+		;;
 	    '!recipe')
 		wordchain Ingredients.TXT 10
 		;;
@@ -60,7 +75,8 @@ tail -f -n 0 out | \
 		exec $0
 		;;
 	    '!source')
-		echo "https://github.com/ringtech/TempleBot"
+		echo "$code"
+		echo "$copying"
 		;;
 	    '!words')
 		wordchain /usr/share/dict/words 10
