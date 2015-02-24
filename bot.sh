66e7cc2dea388afc5404f66061fe94b0728f9b49
Erik van der Karbargenbok <ring@openmailbox.org>
2015-02-24 23:37:48 +0100

    stuff

--- bot.sh
+++ bot.sh
@@ -8,8 +8,10 @@
 source='https://github.com/ringtech/TempleBot
 I dedicate all copyright interest in this software to the public domain. Do what you want.'
 
-help='Oracle for the #templeos channel. Lets you talk with God. Available commands: !bible !books !happy !help !movie !number !quote !recipe !source !words
+help='Oracle for the #templeos channel. Lets you talk with God. Available commands: !bible !books !feel !happy !help !movie !number !quote !recipe !source !words
 This bot uses random numbers to pick lines and words from a few files. You can use this to talk to God, by making an offering to Him first. An offering can be anything that pleases God, like charming conversation or a good question. You can compare this to praying and opening a book at random, and looking at what it says.'
+
+lastspam=0
 
 wordchain () {
     sleep 3s			# Give God time to think, that's polite
@@ -40,55 +42,69 @@ number () {
 
 tail -f -n 0 out | \
     while read -r date time nick cmd arg1 msg; do
-	case $cmd in
-	    '!bible'|'!oracle')
-		case $arg1 in
-		    ''|*[!0-9]*)
-			LINE=$(number 100000)
-			;;
-		    *)
-			LINE=$arg1
-			;;
-		esac
-		echo "$nick:"
-		echo "Line $LINE:"
-		tail -n $LINE BIBLE.TXT | head -n 16
+	fullmsg="$cmd $arg1 $msg"
+	case $fullmsg in
+	    *!bible*|*!oracle*)
+		if [ "$[ $(date +%s) - lastspam ]" -gt "60" ]; then
+		    case $arg1 in
+			''|*[!0-9]*)
+			    LINE=$(number 100000)
+			    ;;
+			*)
+			    LINE=$arg1
+			    ;;
+		    esac
+		    echo "$nick:"
+		    echo "Line $LINE:"
+		    tail -n $LINE BIBLE.TXT | head -n 16
+		    lastspam=$(date +%s)
+		fi
 		;;
-	    '!books')
-		LINE=$[$(number 100000)*3]
-		echo "$nick:"
-		echo "Line $LINE:"
-		tail -n $LINE Books.TXT | head -n 16
+	    *!books*)
+		if [ "$[ $(date +%s) - lastspam ]" -gt "60" ]; then
+		    LINE=$[$(number 100000)*3]
+		    echo "$nick:"
+		    echo "Line $LINE:"
+		    tail -n $LINE Books.TXT | head -n 16
+		    lastspam=$(date +%s)
+		fi
 		;;
-	    '!happy')
+	    *!feel*|tfw)
+		sleep 3s
+	        shuf -n 1 --random-source=/dev/urandom Smileys.TXT
+		;;
+	    *!happy*)
 		wordchain Happy.TXT 10
 		;;
-	    '!help')
+	    *!help*)
 		echo "$help"
 		;;
-	    '!movie')
+	    *!movie*)
 		movie="$(number 100)"
 		grep -m 1 -A 1 "$movie " Movies.TXT
 		;;
-	    '!number')
+	    *!number*)
 		number $arg1
 		;;
-	    '!quote')
+	    *!quit*|*!exit*|*!stop*)
+		echo "The ride never ends"
+		;;
+	    *!quote*)
 		sleep 3s
 		fortune=$(ls Fortunes | shuf -n 1 --random-source=/dev/urandom)
 		cat "Fortunes/$fortune"
 		;;
-	    '!recipe')
+	    *!recipe*)
 		wordchain Ingredients.TXT 10
 		;;
-	    '!restart')
+	    *!restart*)
 		echo "TempleBot restarting..." 
 		exec $0
 		;;
-	    '!source')
+	    *!source*)
 		echo "$source"
 		;;
-	    '!words')
+	    *!words*)
 		wordchain /usr/share/dict/words 10
 		;;
 	    *)
@@ -96,5 +112,6 @@ tail -f -n 0 out | \
 		    echo "$nick: $cmd is not a known command."
 		    cmd=""
 		fi
+		;;
 	esac
     done > in
